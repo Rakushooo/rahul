ccaccc7650da38719b11eab8a4f3e2e45f698e8a
rahul <rahul.sharma>
2022-08-13 13:58:19 +0530

    initial commit

---
+++ src/main/java/test/HelloWeb.java
@@ -0,0 +1,16 @@
+package test;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloWeb extends HttpServlet{
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+		PrintWriter out=res.getWriter();
+		out.print("Hello Web");
+	}
+
+}
